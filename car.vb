5d1fc3f930586f11168ec5224c87b0cbc9779513
WinkingDog885 <alexbarba885@rusdlearns.net>
2015-09-24 14:48:31 -0700

    Create car.vb

---
+++ car.vb
@@ -0,0 +1,15 @@
+Public Class Car
+
+    Public Make As String
+    Public Model As String
+    Public Year As Integer
+    Public Colour As String
+
+
+    Public Function determineMarketValue(ByVal myCar As Car) As Double
+        Return 100.0
+
+
+    End Function
+
+End Class
