8ce2274ff1fe2891a4cdb820594a8e7dca5bef39
WinkingDog885 <alexbarba885@rusdlearns.net>
2015-09-25 14:46:37 -0700

    Update Classes.vb

--- Classes.vb
+++ Classes.vb
@@ -3,6 +3,10 @@ Module Module1
     Sub Main()
         'Dim myNewCar As Car = New Car
         Dim myNewCar As New Car
+        'Dim myOtherCar As Car = myNewCar
+        'Dim myOtherCar As New Car("BMW,2005,30000)
+
+        CarLot.AddToInventory(myNewCar)
 
         myNewCar.Make = "Oldsmobile"
         myNewCar.Model = "Cutlas Supreme"
@@ -15,11 +19,33 @@ Module Module1
         '.Year = "1986"
         '.Colour = "Silver"
         'End With
+
         'Console.WriteLine("{0} - {1} - {2}", myNewCar.Make, myNewCar.Model, myNewCar.Year, myNewCar.Year.ToString())
         'Console.WriteLine(determineMarketValue(myNewCar))
-        Console.WriteLine(myNewCar.determineMarketValue())
+        'Console.WriteLine(myNewCar.determineMarketValue())
+
+        Console.WriteLine("Before:" & myNewCar.Make)
+        doByValue(myNewCar)
+        Console.WriteLine("After By Value:" & myNewCar.Make)
+
+        doByReference(myNewCar)
+        Console.WriteLine("After By Reference:" & myNewCar.Make)
         Console.ReadLine()
 
+    End Sub
+    'Function.determineMarketValue(ByVal myCar as Car) As Double
+    'super secret formula
+
+        'Return 100.0
+
+    Sub doByValue(ByVal myCar As Car)
+        myCar = New Car
+        myCar.Make = "BMW"
+    End Sub
+
+    Sub doByReference(ByRef myCar As Car)
+        myCar = New Car
+        myCar.Make = "BMW"
 
     End Sub
 
