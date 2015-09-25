5dca07d92a5bc2a1c0fadb5e1c93ff7b40eb18bd
WinkingDog885 <alexbarba885@rusdlearns.net>
2015-09-25 14:47:07 -0700

    Update car.vb

--- car.vb
+++ car.vb
@@ -4,12 +4,43 @@ Public Class Car
     Public Model As String
     Public Year As Integer
     Public Colour As String
+    Public Amount As Double
+    Public Surcharge As Double
+    Public Sub New()
 
+    End Sub
+    Public Sub New(ByVal _Make As String, ByVal _Year As Integer, ByVal _Amount As Double)
+        Me.Make = _Make
+        Me.Year = _Year
+        Me.Amount = _Amount
+        Me.Surcharge = _Amount * 1.025
+
+
+    End Sub
 
     Public Function determineMarketValue(ByVal myCar As Car) As Double
         Return 100.0
 
 
     End Function
+
+    Public Sub myMethod(ByVal myParameter As String)
+
+    End Sub
+
+    Public Sub myMethod(ByVal anotherVersion As String, ByVal myParameter As String)
+
+    End Sub
+
+    Public Sub myMethod(ByVal myParameter As Integer)
+        'body of the method goes here
+    End Sub
+
+End Class
+
+Public Class CarLot
+    Public Shared Sub AddToInventory(ByVal newCar As Car)
+
+    End Sub
 
 End Class
